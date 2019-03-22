75955ffea41e2007d7c61bf7c5af55e5b3de0850
raymondmiracle <rmiracle201@gmail.com>
2019-03-22 22:12:52 +0000

    first commit

---
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -0,0 +1,10 @@
+#ifndef __MT65XX_LCM_LIST_H__
+#define __MT65XX_LCM_LIST_H__
+
+#include <lcm_drv.h>
+extern LCM_DRIVER ili9881c_hd720_boe5p2_dj_5080_lcm_drv;
+#ifdef BUILD_LK
+extern void mdelay(unsigned long msec);
+#endif
+
+#endif
